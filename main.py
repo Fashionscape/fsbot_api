ad27bc07103ae14b774f010f646b3053ca0b743e
Alex G <agryczewski@outlook.com>
2023-04-21 21:09:34 -0400

    Prototype hybrid cloud security

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import asyncio
+import msal
 from fastapi import FastAPI, Depends
+
+from azure.keyvault.secrets import SecretClient
+from azure.identity import DefaultAzureCredential
 
 from auth.validate_token_header import validate_token
 from router import outfit
@@ -10,8 +14,20 @@ app = FastAPI(dependencies=[Depends(validate_token)])
 app.include_router(outfit.router)
 
 
+async def azure():
+    cred = DefaultAzureCredential()
+    client = SecretClient(vault_url="https://fashionscape.vault.azure.net/", credential=cred)
+
+    secret_name = "fsbot-discord-token-dev"
+    s = client.get_secret(secret_name)
+
+    print(s.value)
+
+
 async def start():
     await db_config.get()
+
+    await azure()
 
 
 # Press the green button in the gutter to run the script.
